574bcb967bcc146ffab22efb25ec52be5ba10c07
burkus <lucas.burke@pm.me>
2019-05-24 12:46:12 -0700

    updated with draft pt3

--- Project 3/test.asm
+++ Project 3/test.asm
@@ -3,18 +3,21 @@ global _start
 section .text
 _start:
 
+xor r15, r15 ; for the fd
+
 ;open
- push 0x79656b
- mov rdi, rsp
- xor rsi, rsi ; flags = O_RDONLY
- mov rdx, rsi ; mode = NULL
- xor rax, rax
- mov al, 2	; = 2
- syscall
+  push 0x79656b
+  mov rdi, rsp
+  xor rsi, rsi ; flags = O_RDONLY
+  mov rdx, rsi ; mode = NULL
+  xor rax, rax
+  mov rax, 2	; = 2
+  syscall
+  mov r15, rax
   
-loop_in:
+loop_in: 
   ; read syscall args
-  mov rdi, rax ;fd from open
+  mov rdi, r15 ;fd from open
   lea rsi, [rsp-1] ;store a byte at [rsp-1]
   mov rdx, 1
   mov rax, 0 
@@ -26,10 +29,10 @@ loop_in:
 
 loop_out:
   ; write syscall args
-  mov rdi, 1
+  mov rdi, 1 ;write to stdout
   lea rsi, [rsp-1]
   mov rdx, 1
-  mov rax, 1  ; write to stdout
+  mov rax, 1 
   syscall
 
   cmp rax, 1

